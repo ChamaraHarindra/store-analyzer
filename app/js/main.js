4bff0f02b987b85564324f14a6620f6c511e3249
REDCORPSL\chamarah <chamara.harindra@dev.cms.lk>
2018-06-21 00:31:33 +0530

    new changes from office

--- app/js/main.js
+++ app/js/main.js
@@ -2,31 +2,37 @@
 var screenWidth = window.innerWidth;
 
 if (screenWidth <= 768) {
-    document.getElementById('logo-main').src = "./images/logo-small.png";
+  document.getElementById('logo-main').src = './images/logo-small.png';
 }
 
 // Calendar widget
-window.addEventListener('load', function () {
-    vanillaCalendar.init({
-        disablePastDays: true
-    });
-})
-
-
-$(document).ready(function () {
-    $("#sidebar").mCustomScrollbar({
-        theme: "minimal"
-    });
-
-    $('#sidebarCollapse').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
-        $('.collapse.in').toggleClass('in');
-        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-    });
-
-    $('#asideClose').on('click', function () {
-        $('#sidebar, #content').toggleClass('active');
-    });
+window.addEventListener('load', function() {
+  vanillaCalendar.init({
+    disablePastDays: true
+  });
 });
 
+$(document).ready(function() {
+  $('#sidebar').mCustomScrollbar({
+    theme: 'minimal'
+  });
 
+  $('#sidebarCollapse').on('click', function() {
+    $('#sidebar, #content').toggleClass('active');
+    $('.collapse.in').toggleClass('in');
+    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+  });
+
+  $('#asideClose').on('click', function() {
+    $('#sidebar, #content').toggleClass('active');
+  });
+});
+
+$(function() {
+  $('#wizard').steps({
+    headerTag: 'h2',
+    bodyTag: 'section', 
+    transitionEffect: 'slideLeft',
+    stepsOrientation: 'vertical'
+  });
+});
